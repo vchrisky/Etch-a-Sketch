76242ed14ccdeea4fbdfbfe6fdc1d785a7c0ed81
vchrisky <143357101+vchrisky@users.noreply.github.com>
2023-10-11 23:16:41 +0300

    created grid of square divs and made it respond to mouseover

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let divContainer = document.querySelector('.div-container');
 
-let numRows = 4;
-let numColumns = 4;
+let numRows = 100;
+let numColumns = 100;
+let numContent = 1;
 
-for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < numColumns; j++) {
+
+for (let i = 0; i < numColumns; i++) {
+    let squareRow = document.createElement('div');
+    squareRow.classList.add('grid-columns');
+    divContainer.appendChild(squareRow);
+
+    for (let j = 0; j < numRows; j++) {
         let squareDiv = document.createElement('div');
+        // squareDiv.textContent = `${numContent}`;
         squareDiv.classList.add('grid-square');
-        divContainer.appendChild(squareDiv);
+        squareRow.appendChild(squareDiv);
+        // numContent++;
     }
 }
+let square = document.querySelectorAll('.grid-square');
 
+square.forEach(square => {
+    square.addEventListener('mouseover', () => {
+        square.classList.add('grid-square-hover');    
+    })
+});
