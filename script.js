ea67a6845809715333eb074eb1bc4dc28add78d9
vchrisky <143357101+vchrisky@users.noreply.github.com>
2023-10-12 14:35:04 +0300

    completed event listener for the 'enter sides' button and limited user input to 100

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-let divContainer = document.querySelector('.div-container');
-
-let numRows = 100;
-let numColumns = 100;
-let numContent = 1;
-
-
-for (let i = 0; i < numColumns; i++) {
-    let squareRow = document.createElement('div');
-    squareRow.classList.add('grid-columns');
-    divContainer.appendChild(squareRow);
-
-    for (let j = 0; j < numRows; j++) {
-        let squareDiv = document.createElement('div');
-        // squareDiv.textContent = `${numContent}`;
-        squareDiv.classList.add('grid-square');
-        squareRow.appendChild(squareDiv);
-        // numContent++;
+function createGrid(numColumns, numRows) {
+    for (let i = 0; i < numColumns; i++) {
+        let squareRow = document.createElement('div');
+        squareRow.classList.add('grid-column');
+        divContainer.appendChild(squareRow);
+    
+        for (let j = 0; j < numRows; j++) {
+            let squareDiv = document.createElement('div');
+            squareDiv.classList.add('grid-square');
+            squareRow.appendChild(squareDiv);
+        }
     }
 }
-let square = document.querySelectorAll('.grid-square');
 
-square.forEach(square => {
+let divContainer = document.querySelector('.div-container');
+let displaySides = document.querySelector('.display-sides');
+
+let numColumns = 16;
+let numRows = 16;
+
+createGrid(numColumns, numRows);
+displaySides.textContent = `${numColumns} * ${numRows}`;
+
+// Add an event listener to change the class for every square.
+let squares = document.querySelectorAll('.grid-square');
+
+squares.forEach(square => {
     square.addEventListener('mouseover', () => {
         square.classList.add('grid-square-hover');    
     })
 });
+
+let btnContainer = document.querySelector('.button-container');
+let numSidesBtn = document.createElement('button');
+
+numSidesBtn.textContent = 'Enter Number of Sides';
+btnContainer.appendChild(numSidesBtn);
+
+numSidesBtn.addEventListener('click', () => {
+    let userSides = +prompt('How many sides wud u Like?', '16');
+
+    if (userSides > 100) {
+        alert('The maximum limit is 100! Please enter a value that is not past 100');
+    }else if (userSides < 1){
+        alert('The minimum limit is 1! Please enter a value that is not less than 1');
+    }else{
+    divContainer.innerHTML = '';
+    let numRows=numColumns=userSides;
+    createGrid(numColumns, numRows);
+    displaySides.textContent = `${numColumns} * ${numRows}`;
+    }
+    
+    let squares = document.querySelectorAll('.grid-square');
+    squares.forEach(square => {
+      square.addEventListener('mouseover', () => {
+        square.classList.add('grid-square-hover');    
+      })
+    });  
+})
