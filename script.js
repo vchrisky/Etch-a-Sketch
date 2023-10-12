f74fc30c94af7a4cce7e6d2144e86fdc7fd58700
vchrisky <143357101+vchrisky@users.noreply.github.com>
2023-10-12 15:57:31 +0300

    added a button to randomize color

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let numColumns = 16;
 let numRows = 16;
 
 createGrid(numColumns, numRows);
-displaySides.textContent = `${numColumns} * ${numRows}`;
+displaySides.textContent = `GRID: ${numColumns} * ${numRows}`;
 
 // Add an event listener to change the class for every square.
 let squares = document.querySelectorAll('.grid-square');
@@ -47,13 +47,29 @@ numSidesBtn.addEventListener('click', () => {
     divContainer.innerHTML = '';
     let numRows=numColumns=userSides;
     createGrid(numColumns, numRows);
-    displaySides.textContent = `${numColumns} * ${numRows}`;
+    displaySides.textContent = `GRID: ${numColumns} * ${numRows}`;
     }
     
     let squares = document.querySelectorAll('.grid-square');
     squares.forEach(square => {
       square.addEventListener('mouseover', () => {
         square.classList.add('grid-square-hover');    
+      })
+    });  
+})
+
+let randomRgbBtn = document.createElement('button');
+randomRgbBtn.textContent = 'Randomize Color';
+btnContainer.appendChild(randomRgbBtn);
+
+randomRgbBtn.addEventListener('click', () => {
+    let squares = document.querySelectorAll('.grid-square');
+    squares.forEach(square => {
+      square.addEventListener('mouseover', () => {
+        rgbValue1 = Math.floor(Math.random()*255);
+        rgbValue2 = Math.floor(Math.random()*255);
+        rgbValue3 = Math.floor(Math.random()*255);
+        square.style.cssText = `background-color: rgb(${rgbValue1}, ${rgbValue2}, ${rgbValue3});`;    
       })
     });  
 })
